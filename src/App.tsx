a831976092f08ded3166e32c1f89bfb8b4b5a544
Paloma Wink <paloma.wink@hotmail.com>
2023-12-22 16:45:08 -0300

    feat: uniao de tudo no componente principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
+import styles from './App.module.css'
 
 function App() {
 
   return (
     <div>
-      <h1>TODO React + TypeScript</h1>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que voce vai fazer?</h2>
+          <TaskForm />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
+      </main>
+      <Footer />
     </div>
   )
 }
