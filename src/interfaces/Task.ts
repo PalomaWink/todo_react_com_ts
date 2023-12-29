3b080d3d0c100bed3e746043f18ec40e5afb93e2
Paloma Wink <paloma.wink@hotmail.com>
2023-12-29 18:04:40 -0300

    feat: Criando interface para componente TaskList

--- src/interfaces/Task.ts
+++ src/interfaces/Task.ts
@@ -6,7 +6,11 @@ export interface ITask {
 
 // a tipagen desse setTaskList é um dispatch do React, onde eu disparo um atualização de estado (setState) e essa atualização
 // esta trabalhando com um array de tarefas (ITask[])
-export interface Props {
+export interface PropsTaskForm {
   taskList: ITask[],
   setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+}
+
+export interface PropsTaskList {
+  taskList: ITask[],
 }
