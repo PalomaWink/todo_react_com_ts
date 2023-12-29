70f5d0cb6cd2fac78797f03221085fdaf90d5964
Paloma Wink <paloma.wink@hotmail.com>
2023-12-29 17:13:01 -0300

    feat: Criando interface para as props do TaskForm

--- src/interfaces/Task.ts
+++ src/interfaces/Task.ts
@@ -3,3 +3,10 @@ export interface ITask {
   title: string,
   difficulty: number,
 }
+
+// a tipagen desse setTaskList é um dispatch do React, onde eu disparo um atualização de estado (setState) e essa atualização
+// esta trabalhando com um array de tarefas (ITask[])
+export interface Props {
+  taskList: ITask[],
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+}
