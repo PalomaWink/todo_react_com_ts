8cebc821d6ebc96ab4452fb22de2dfe04b370701
Paloma Wink <paloma.wink@hotmail.com>
2024-01-03 20:40:15 -0300

    feat: Funcionalidade do lapis para abrir edicao de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import styles from './App.module.css'
 
 //Interfaces
 import { ITask } from './interfaces/Task'
+import Modal from './components/Modal'
 
 function App() {
 
@@ -18,17 +19,32 @@ function App() {
   const deleteTask = (id: number): void => {
     setTaskList(taskList.filter((task: ITask) => task.id !== id))
   }
+
+  const hideOrShowModal = (display: boolean): void => {
+    const modal = document.querySelector('#modal')
+    if (display) {
+      modal!.classList.remove("hide")
+    } else {
+      modal!.classList.add("hide")
+    }
+  }
+
+  const editTask = (): void => {
+    hideOrShowModal(true)
+  }
+
   return (
     <div>
+      <Modal children={<TaskForm taskList={taskList} btnText='Editar tarefa' />} />
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que voce vai fazer?</h2>
-          <TaskForm taskList={taskList} setTaskList={setTaskList}/>
+          <TaskForm taskList={taskList} setTaskList={setTaskList} btnText='Criar tarefa'/>
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} handleDeleteTask={deleteTask} />
+          <TaskList taskList={taskList} handleDeleteTask={deleteTask} handleEdit={editTask} />
         </div>
       </main>
       <Footer />
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,7 +4,7 @@ import { PropsTaskList } from "../interfaces/Task"
 // CSS
 import styles from './TaskList.module.css'
 
-const TaskList = ( {taskList, handleDeleteTask}: PropsTaskList) => {
+const TaskList = ( {taskList, handleDeleteTask, handleEdit}: PropsTaskList) => {
   return (
     <>
       {taskList.length > 0 ? (
@@ -15,7 +15,7 @@ const TaskList = ( {taskList, handleDeleteTask}: PropsTaskList) => {
               <p>Dificuldade: {task.difficulty}</p>
             </div>
             <div className={styles.actions}>
-              <i className="bi bi-pencil-square"></i>
+              <i className="bi bi-pencil-square" onClick={() => handleEdit()}></i>
               <i className="bi bi-trash3" onClick={() => handleDeleteTask(task.id)}></i>
             </div>
           </div>
